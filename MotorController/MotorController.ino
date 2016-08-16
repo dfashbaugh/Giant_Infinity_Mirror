d92aeea7bd9759e4e6a34745d8a26b411a9154dd
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-08-15 22:30:15 -0400

    Changed motors to use bottom, left, and right rather than corner based positions

--- MotorController/MotorController.ino
+++ MotorController/MotorController.ino
@@ -1,20 +1,9 @@
-/* 
-This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
-It won't work with v1.x motor shields! Only for the v2's with built in PWM
-control
-
-For use with the Adafruit Motor Shield v2 
----->	http://www.adafruit.com/products/1438
-*/
-
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-// Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
 
 struct MotorAndPosition
 {
@@ -22,22 +11,17 @@ struct MotorAndPosition
   int destination;
   int curPosition;
   int velocity;
+  int pGain;
 };
 
 // Select which 'port' M1, M2, M3 or M4. In this case, M1
-Adafruit_DCMotor *bottomLeftMotor = AFMS.getMotor(3);
-// You can also make another motor on port M2
-Adafruit_DCMotor *bottomRightMotor = AFMS.getMotor(4);
-Adafruit_DCMotor *topLeftMotor = AFMS.getMotor(2);
-Adafruit_DCMotor *topRightMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *bottomMotor = AFMS.getMotor(3);
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(2);
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(1);
 
-MotorAndPosition bottomLeft;
-MotorAndPosition bottomRight;
-MotorAndPosition topLeft;
-MotorAndPosition topRight;
-
-// P Controller gain
-int p = 10;
+MotorAndPosition bottom;
+MotorAndPosition left;
+MotorAndPosition right;
 
 void initialize(Adafruit_DCMotor *motor)
 {
@@ -57,36 +41,20 @@ void setup() {
 
   AFMS.begin();  // create with the default frequency 1.6KHz
   
-  bottomLeft.motor = bottomLeftMotor;
-  bottomLeft.destination = 0;
-  bottomRight.motor = bottomRightMotor;
-  bottomRight.destination = 0;
-  topLeft.motor = topLeftMotor;
-  topLeft.destination = 0;
-  topRight.motor = topRightMotor;
-  topRight.destination = 0;
+  bottom.motor = bottomMotor;
+  bottom.destination = 0;
+  left.motor = leftMotor;
+  left.destination = 0;
+  right.motor = rightMotor;
+  right.destination = 0;
 
-  initialize(bottomLeft.motor);
-  initialize(bottomRight.motor);
-  initialize(topLeft.motor);
-  initialize(topRight.motor);
+  initialize(bottom.motor);
+  initialize(left.motor);
+  initialize(right.motor);
   delay(12000);
-  topRight.motor->run(RELEASE);
-  topLeft.motor->run(RELEASE);
-  bottomRight.motor->run(RELEASE);
-  bottomLeft.motor->run(RELEASE);
+  stopAll();
 
   driveToCenter();
-  driveCircles();
-
-  //driveForwardTime(topRight, 1000);
-
-  // Set the speed to start, from 0 (off) to 255 (max speed)
-  //myMotor->setSpeed(150);
-  //myMotor->run(FORWARD);
-  // turn on motor
-  //myMotor->run(RELEASE);
-  //myMotor->run(BACKWARD);
 }
 
 void MoveMotor(MotorAndPosition &curMotor)
@@ -101,7 +69,7 @@ void MoveMotor(MotorAndPosition &curMotor)
     return;
   }
 
-  int velocity = positionError*p;
+  int velocity = positionError*curMotor.pGain;
   curMotor.motor->setSpeed(velocity);
   if(positionError < 0)
     curMotor.motor->run(BACKWARD);
@@ -140,43 +108,16 @@ void driveForward(MotorAndPosition &curMotor)
 
 void stopAll()
 {
-  topLeft.motor->run(RELEASE);
-  bottomRight.motor->run(RELEASE);
-  topRight.motor->run(RELEASE);
-  bottomLeft.motor->run(RELEASE);
+  left.motor->run(RELEASE);
+  bottom.motor->run(RELEASE);
+  right.motor->run(RELEASE);
 }
 
 void driveToCenter()
 {
-  driveBackward(topLeft);
-  driveBackward(topRight);
-  driveBackward(bottomLeft);
-  driveBackward(bottomRight);
-  delay(5000);
-  stopAll();
-}
-
-void driveCircles()
-{
-  driveBackward(topLeft);
-  driveForward(bottomRight);
-  delay(5000);
-  driveForward(topLeft);
-  driveBackward(bottomRight);
-  delay(10000);
-  driveBackward(topLeft);
-  driveForward(bottomRight);
-  delay(5000);
-  stopAll();
-  
-  driveBackward(topRight);
-  driveForward(bottomLeft);
-  delay(5000);
-  driveForward(topRight);
-  driveBackward(bottomLeft);
-  delay(10000);
-  driveBackward(topRight);
-  driveForward(bottomLeft);
+  driveBackward(left);
+  driveBackward(right);
+  driveBackward(bottom);
   delay(5000);
   stopAll();
 }
