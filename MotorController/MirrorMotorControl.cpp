c2af2d5890541fd06dcb0909c3989bcdbd4e6906
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-08-16 08:46:38 -0400

    Created Mirror Motor Control class

---
+++ MotorController/MirrorMotorControl.cpp
@@ -0,0 +1,117 @@
+#include "MirrorMotorControl.h"
+
+void MirrorMotorControl::SetupMotors()
+{
+  AFMS = Adafruit_MotorShield(); 
+  AFMS.begin();  
+  
+  bottomMotor = AFMS.getMotor(1);;
+  bottom.motor = bottomMotor;
+  bottom.destination = 0;
+
+  leftMotor = AFMS.getMotor(2);
+  left.motor = leftMotor;
+  left.destination = 0;
+
+  rightMotor = AFMS.getMotor(3);
+  right.motor = rightMotor;
+  right.destination = 0;
+
+  initialize(bottom.motor);
+  initialize(left.motor);
+  initialize(right.motor);
+  delay(12000);
+  stopAll();
+
+  driveToCenter();
+}
+
+void MirrorMotorControl::MoveMotor(MotorAndPosition &curMotor)
+{
+  curMotor.curPosition = 0; // Read in the position
+
+  int positionError = curMotor.curPosition - curMotor.destination;
+
+  if(positionError == 0)
+  {
+    curMotor.motor->run(RELEASE);
+    return;
+  }
+
+  int velocity = positionError*curMotor.pGain;
+  curMotor.motor->setSpeed(velocity);
+  if(positionError < 0)
+    curMotor.motor->run(BACKWARD);
+  else
+    curMotor.motor->run(FORWARD);
+}
+
+void MirrorMotorControl::initialize(Adafruit_DCMotor *motor)
+{
+  motor->setSpeed(255);
+  motor->run(FORWARD);
+}
+
+void MirrorMotorControl::reverseInitialize(Adafruit_DCMotor *motor)
+{
+  motor->setSpeed(255);
+  motor->run(BACKWARD);
+}
+
+void MirrorMotorControl::driveForwardTime(MotorAndPosition &curMotor, int delayTime)
+{
+  curMotor.motor->setSpeed(255);
+  curMotor.motor->run(FORWARD);
+  delay(delayTime);
+  curMotor.motor->run(RELEASE);
+}
+
+void MirrorMotorControl::driveBackwardTime(MotorAndPosition &curMotor, int delayTime)
+{
+    curMotor.motor->setSpeed(255);
+    curMotor.motor->run(BACKWARD);
+    delay(delayTime);
+    curMotor.motor->run(RELEASE);
+}
+
+void MirrorMotorControl::driveBackward(MotorAndPosition &curMotor)
+{
+    curMotor.motor->setSpeed(255);
+    curMotor.motor->run(BACKWARD);
+}
+
+void MirrorMotorControl::driveForward(MotorAndPosition &curMotor)
+{
+  curMotor.motor->setSpeed(255);
+  curMotor.motor->run(FORWARD);
+}
+
+void MirrorMotorControl::stopAll()
+{
+  left.motor->run(RELEASE);
+  bottom.motor->run(RELEASE);
+  right.motor->run(RELEASE);
+}
+
+void MirrorMotorControl::driveToCenter()
+{
+  driveBackward(left);
+  driveBackward(right);
+  driveBackward(bottom);
+  delay(5000);
+  stopAll();
+}
+
+void MirrorMotorControl::MoveToXYZ(int x, int y, int z)
+{
+  bottom.destination = y + z;
+  left.destination = x + z;
+  right.destination = -x + z;
+}
+
+void MirrorMotorControl::MoveMotors()
+{
+  MoveMotor(bottom);
+  MoveMotor(left);
+  MoveMotor(right);
+}
--- MotorController/MotorController.ino
+++ MotorController/MotorController.ino
@@ -1,28 +1,9 @@
 #include <Wire.h>
-#include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_MS_PWMServoDriver.h"
+#include "MirrorMotorControl.h"
 
-// Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+MirrorMotorControl theController;
 
-struct MotorAndPosition
-{
-  Adafruit_DCMotor* motor;
-  int destination;
-  int curPosition;
-  int velocity;
-  int pGain;
-};
-
-// Select which 'port' M1, M2, M3 or M4. In this case, M1
-Adafruit_DCMotor *bottomMotor = AFMS.getMotor(3);
-Adafruit_DCMotor *leftMotor = AFMS.getMotor(2);
-Adafruit_DCMotor *rightMotor = AFMS.getMotor(1);
-
-MotorAndPosition bottom;
-MotorAndPosition left;
-MotorAndPosition right;
-
+// MIDI Control Code
 enum controlEnum{moveY = 1, moveX = 2, moveZ = 3, LEDEffect = 4};
 int  currentYDestination = 0;
 int  currentXDestination = 0;
@@ -44,123 +25,22 @@ void OnControlChange(byte channel, byte control, byte value) {
     currentZDestination = value;
   }
 
-  MoveToXYZ(currentXDestination, currentYDestination, currentZDestination);
- 
+  theController.MoveToXYZ(currentXDestination, currentYDestination, currentZDestination);
 }
 
 void setup() {
-  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.begin(9600);           
   Serial.println("Adafruit Motorshield v2 - DC Motor test!");
 
-  AFMS.begin();  // create with the default frequency 1.6KHz
-  
-  bottom.motor = bottomMotor;
-  bottom.destination = 0;
-  left.motor = leftMotor;
-  left.destination = 0;
-  right.motor = rightMotor;
-  right.destination = 0;
-
-  initialize(bottom.motor);
-  initialize(left.motor);
-  initialize(right.motor);
-  delay(12000);
-  stopAll();
-
-  driveToCenter();
+  theController.SetupMotors();
 
   usbMIDI.setHandleControlChange(OnControlChange);
 }
 
-void MoveMotor(MotorAndPosition &curMotor)
-{
-  curMotor.curPosition = 0; // Read in the position
-
-  int positionError = curMotor.curPosition - curMotor.destination;
-
-  if(positionError == 0)
-  {
-    curMotor.motor->run(RELEASE);
-    return;
-  }
-
-  int velocity = positionError*curMotor.pGain;
-  curMotor.motor->setSpeed(velocity);
-  if(positionError < 0)
-    curMotor.motor->run(BACKWARD);
-  else
-    curMotor.motor->run(FORWARD);
-
-}
-
-void initialize(Adafruit_DCMotor *motor)
-{
-  motor->setSpeed(255);
-  motor->run(FORWARD);
-}
-
-void reverseInitialize(Adafruit_DCMotor *motor)
-{
-  motor->setSpeed(255);
-  motor->run(BACKWARD);
-}
-
-void driveForwardTime(MotorAndPosition &curMotor, int delayTime)
-{
-  curMotor.motor->setSpeed(255);
-  curMotor.motor->run(FORWARD);
-  delay(delayTime);
-  curMotor.motor->run(RELEASE);
-}
-
-void driveBackwardTime(MotorAndPosition &curMotor, int delayTime)
-{
-    curMotor.motor->setSpeed(255);
-    curMotor.motor->run(BACKWARD);
-    delay(delayTime);
-    curMotor.motor->run(RELEASE);
-}
-
-void driveBackward(MotorAndPosition &curMotor)
-{
-    curMotor.motor->setSpeed(255);
-    curMotor.motor->run(BACKWARD);
-}
-
-void driveForward(MotorAndPosition &curMotor)
-{
-  curMotor.motor->setSpeed(255);
-  curMotor.motor->run(FORWARD);
-}
-
-void stopAll()
-{
-  left.motor->run(RELEASE);
-  bottom.motor->run(RELEASE);
-  right.motor->run(RELEASE);
-}
-
-void driveToCenter()
-{
-  driveBackward(left);
-  driveBackward(right);
-  driveBackward(bottom);
-  delay(5000);
-  stopAll();
-}
-
-void MoveToXYZ(int x, int y, int z)
-{
-  bottom.destination = y + z;
-  left.destination = x + z;
-  right.destination = -x + z;
-}
 
 void loop() {
 
-  MoveMotor(bottom);
-  MoveMotor(left);
-  MoveMotor(right);
+  theController.MoveMotors();
 }
 
 
